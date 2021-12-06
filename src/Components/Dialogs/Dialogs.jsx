dfe11174a75f185c3b260ca2cfc038bd274eb187
TarasCoder <4kaskad@gmail.com>
2021-12-07 00:04:52 +0200

    adding pressing button functionality by virtual Dom ref

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -6,14 +6,30 @@ import s from "./Dialogs.module.css";
 function Dialogs(props) {
   let msgData = props.state.messages;
   let dialogsData = props.state.dialogs;
+  let newMessage = React.createRef();
+
+  function handleClick() {
+    console.log(newMessage.current.value)
+  }
+
   return (
     <div>
       <div className={s.dialogs}>
         <div className={s.dialogsItems}>
-          {dialogsData.map(n => <DialogItem name={n.name} id={n.id} /> )}
+          {dialogsData.map((n) => (
+            <DialogItem name={n.name} id={n.id} />
+          ))}
         </div>
         <div className={s.messages}>
-          {msgData.map(m => <Message message={m.message} id={m.id} />)}
+          {msgData.map((m) => (
+            <Message message={m.message} id={m.id} />
+          ))}
+          <div>
+            <textarea ref={newMessage} cols="30" rows="5"></textarea>
+            <div>
+              <button onClick={handleClick}>Add message</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -3,10 +3,24 @@ import Post from "./Post/Post";
 import s from "./MyPosts.module.css";
 
 function MyPosts(props) {
-let postData = props.posts;
+  let postData = props.posts;
+  let newPostElement = React.createRef();
+
+  function handleClick() {
+    let text = newPostElement.current.value;
+    console.log(text);
+  }
   return (
     <div className={s.postBlock}>
       <h3>My Posts</h3>
+      <div className={s.inputZone}>
+        <div>
+          <textarea ref={newPostElement} cols="25" rows="4"></textarea>
+        </div>
+        <div>
+          <button onClick={handleClick}>Add post</button>
+        </div>
+      </div>
       <div>
         {postData.map((p) => (
           <Post message={p.message} />
